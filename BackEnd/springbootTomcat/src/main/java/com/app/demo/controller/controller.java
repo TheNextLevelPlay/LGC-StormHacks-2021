c47c4bda7d74fbef660d9605277d1f1db19998d4
unknown <williamtj2001@gmail.com>
2021-02-20 13:32:27 -0800

    init model and controller classes, created skeleton code

--- BackEnd/springbootTomcat/src/main/java/com/app/demo/controller/controller.java
+++ BackEnd/springbootTomcat/src/main/java/com/app/demo/controller/controller.java
@@ -1,9 +1,13 @@
 package com.app.demo.controller;
 
+import com.app.demo.model.User;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 
 @RestController
 public class controller {
-    
+    private ArrayList<User> userArrayList;
+
 }
