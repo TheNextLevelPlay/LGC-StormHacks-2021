6d065701ae7eb9e81acaeffa6dd37420d6029d05
AzureCrystal <email4keanu@gmail.com>
2021-02-20 14:31:20 -0800

    Added poll.py and implemented poll command

--- FrontEnd/src/main.py
+++ FrontEnd/src/main.py
@@ -12,7 +12,7 @@ client.remove_command("help")
 
 token = os.getenv('DISCORD_TOKEN')
 
-extensionList = ['cogs.ask']
+extensionList = ['cogs.ask', 'cogs.poll']
 
 if __name__ == '__main__':
     for extension in extensionList:
