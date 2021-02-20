b9974180ea710f5bba4779a2b2eb8c682641c075
unknown <williamtj2001@gmail.com>
2021-02-20 14:33:10 -0800

    created the getmapping for userList, Post mapping for suggestion

--- BackEnd/springbootTomcat/src/main/java/com/app/demo/controller/controller.java
+++ BackEnd/springbootTomcat/src/main/java/com/app/demo/controller/controller.java
@@ -1,13 +1,59 @@
 package com.app.demo.controller;
 
 import com.app.demo.model.User;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @RestController
 public class controller {
     private ArrayList<User> userArrayList;
+
+    @PostConstruct
+    public void initIT() throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        userArrayList = new ArrayList<>(Arrays.asList(
+                mapper.readValue(
+                        Paths.get("src/main/resources/data/query.json").toFile(),
+                        User[].class
+                )
+        ));
+    }
+
+    @GetMapping("/api/getUserList")
+    public ArrayList<User> getUserArrayList(HttpServletResponse response) throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(200);
+        return userArrayList;
+    }
+
+    @PostMapping("/api/addSuggestion/{id}")
+    public void addSuggestion(@PathVariable String id, @RequestBody String query, HttpServletResponse response)  throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        Boolean foundID = false;
+        for (int i =0 ; i < userArrayList.size();i++){
+            if (userArrayList.get(i).getId().equals(id)){
+                foundID = true;
+                userArrayList.get(i).getSuggestArrList().add(query);
+            }
+        }
+        if (!foundID){
+            ArrayList<String> suggestArrList = new ArrayList<>();
+            ArrayList<String> askArrList = new ArrayList<>();
+            suggestArrList.add(query);
+            User user = new User(id, askArrList, suggestArrList);
+            userArrayList.add(user);
+        }
+
+        mapper.writeValue(Paths.get("src/main/resources/data/query.json").toFile(), userArrayList);
+        response.setStatus(201);
+    }
 
 }
--- BackEnd/springbootTomcat/src/main/java/com/app/demo/model/User.java
+++ BackEnd/springbootTomcat/src/main/java/com/app/demo/model/User.java
@@ -2,14 +2,55 @@ package com.app.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class User {
     private String id;
+    private ArrayList<String> suggestArrList;
+    private ArrayList<String> askArrList;
+    @JsonProperty("suggestSize")
+    private int suggestArrListSize;
+    @JsonProperty("askSize")
+    private int askArrListSize;
 
-    public User(@JsonProperty("id")String id){
+    public User(@JsonProperty("id")String id, @JsonProperty("ask")ArrayList<String>askArrList, @JsonProperty("suggest")ArrayList<String>suggestArrList){
         this.id = id;
+        this.suggestArrList = suggestArrList;
+        this.askArrList = askArrList;
+        this.suggestArrListSize = suggestArrList.size();
+        this.askArrListSize = askArrList.size();
     }
 
     public String getId(){
         return this.id;
     }
+
+    public ArrayList<String> getSuggestArrList(){
+        return this.suggestArrList;
+    }
+
+    public ArrayList<String> getAskArrList(){
+        return this.getAskArrList();
+    }
+
+    public void addSuggest(String query){
+        this.suggestArrList.add(query);
+        this.suggestArrListSize++;
+    }
+
+    public void addAsk(String query){
+        this.askArrList.add(query);
+        this.askArrListSize++;
+    }
+
+    public void removeSuggest(){
+
+    }
+
+    public void removeAsk(){
+
+    }
+
+
 }
