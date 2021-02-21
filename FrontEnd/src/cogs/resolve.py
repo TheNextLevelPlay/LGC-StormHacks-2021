1f7f2bf9636e96b9462a8fbecdeeb62fa8f8efe3
AzureCrystal <email4keanu@gmail.com>
2021-02-20 22:19:12 -0800

    Added admin check on resolve commands

--- FrontEnd/src/cogs/resolve.py
+++ FrontEnd/src/cogs/resolve.py
@@ -25,46 +25,52 @@ class Resolve(commands.Cog):
     @commands.command()
     async def resolveSuggest(self, ctx, *args):
         try:
-            if len(args) != 0:
-                converter = MemberConverter()
-                usrName = await converter.convert(ctx, args[0])
-                usrId = usrName.id
-                if len(args) == 1:
-                    await ctx.send("Resolved " + str(usrName) + "'s suggestions")
-                    postResolveSuggestAll(usrId)
-                elif len(args) == 2:
-                    if self.is_integer(args[1]) is True:
-                        await ctx.send("Resolved " + str(usrName) + "'s suggestion index: " + str(args[1]))
-                        postResolveSuggest(args[1], usrId)
+            if ctx.message.author.guild_permissions.administrator:
+                if len(args) != 0:
+                    converter = MemberConverter()
+                    usrName = await converter.convert(ctx, args[0])
+                    usrId = usrName.id
+                    if len(args) == 1:
+                        await ctx.send("Resolved " + str(usrName) + "'s suggestions")
+                        postResolveSuggestAll(usrId)
+                    elif len(args) == 2:
+                        if self.is_integer(args[1]) is True:
+                            await ctx.send("Resolved " + str(usrName) + "'s suggestion index: " + str(args[1]))
+                            postResolveSuggest(args[1], usrId)
+                        else:
+                            await ctx.send("Invalid resolve command!")
                     else:
                         await ctx.send("Invalid resolve command!")
                 else:
                     await ctx.send("Invalid resolve command!")
             else:
-                await ctx.send("Invalid resolve command!")
+                await ctx.send("You do not have permissions to do this.")
         except:
             await ctx.send("Invalid user!")
 
     @commands.command()
     async def resolveAsk(self, ctx, *args):
         try:
-            if len(args) != 0:
-                converter = MemberConverter()
-                usrName = await converter.convert(ctx, args[0])
-                usrId = usrName.id
-                if len(args) == 1:
-                    await ctx.send("Resolved " + str(usrName) + "'s question")
-                    postResolveAskAll(usrId)
-                elif len(args) == 2:
-                    if self.is_integer(args[1]) is True:
-                        await ctx.send("Resolved " + str(usrName) + "'s question index: " + str(args[1]))
-                        postResolveAsk(args[1], usrId)
+            if ctx.message.author.guild_permissions.administrator:
+                if len(args) != 0:
+                    converter = MemberConverter()
+                    usrName = await converter.convert(ctx, args[0])
+                    usrId = usrName.id
+                    if len(args) == 1:
+                        await ctx.send("Resolved " + str(usrName) + "'s question")
+                        postResolveAskAll(usrId)
+                    elif len(args) == 2:
+                        if self.is_integer(args[1]) is True:
+                            await ctx.send("Resolved " + str(usrName) + "'s question index: " + str(args[1]))
+                            postResolveAsk(args[1], usrId)
+                        else:
+                            await ctx.send("Invalid resolve command!")
                     else:
                         await ctx.send("Invalid resolve command!")
                 else:
                     await ctx.send("Invalid resolve command!")
             else:
-                await ctx.send("Invalid resolve command!")
+                await ctx.send("You do not have permissions to do this.")
         except:
                 await ctx.send("Invalid user!")
 
